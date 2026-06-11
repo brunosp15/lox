8b554a4ce5eadcf127de0a738d69844069e3a0ed
Bruno Pinto <brunosp15@gmail.com>
2026-06-11 09:08:32 -0300

    Finishing JLOX and part II, LET'S GO TO THE FUN STUFF

--- src/main/java/com/craftinginterpreters/lox/LoxClass.java
+++ src/main/java/com/craftinginterpreters/lox/LoxClass.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 class LoxClass implements LoxCallable {
+    final LoxClass superclass;
     final String name;
     private final Map<String, LoxFunction> methods;
 
-    LoxClass(String name, Map<String, LoxFunction> methods) {
+    LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
+        this.superclass = superclass;
         this.name = name;
         this.methods = methods;
     }
@@ -16,7 +18,9 @@ class LoxClass implements LoxCallable {
         if (methods.containsKey(name)) {
             return methods.get(name);
         }
-
+        if (superclass != null) {
+            return superclass.findMethod(name);
+        }
         return null;
     }
 
